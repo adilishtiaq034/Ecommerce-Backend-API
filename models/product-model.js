3cad17b5f346435a6706ff9517394e9bf5c1bc28
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-09 19:00:46 +0500

    Setting the enviroment

---
+++ models/product-model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+
+           name:{
+                   type:String,
+                   required:true,
+                   unique:true
+
+           },
+           price:{
+                   type:Number,
+                   required:true,
+                   min:0
+           },
+           description:{
+                   type:String,
+                   required:true,
+                   maxlength:200
+           },
+           category:{
+                   type:String,
+                   required:true
+           },
+           stock:{
+                type:Number,
+                required:true,
+                min:0
+           }},
+{ 
+    timestamps : true
+})
+
+const Product = mongoose.model("Product",productSchema)
+
+module.exports = Product
