49216f8ea0e50b71a26664d88da27e0080c8d792
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-11 15:21:08 +0500

    2.1.0

--- routers/order-routes.js
+++ routers/order-routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createorder } = require('../controllers/order-controller')
+const { createorder,getOrders } = require('../controllers/order-controller')
 const authmiddleware = require('../middlewares/auth-middleware')
 
 router.post('/create', authmiddleware, createorder)
+router.get('/get', authmiddleware, getOrders)
 
 module.exports = router
