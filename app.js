48dab48ca1df59472af56c086e09c91687b51a67
Adil Ishtiaq <adilishtiaq034@gmail.com>
2026-08-18 18:43:29 +0500

    Implementing Rate Limiting

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const helmet = require('helmet')
 app.use(helmet())
 const cors = require('cors')
 app.use(cors())
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // maximum 100 requests per windowMs
+})
+app.use(limiter)
 require('dotenv').config()
 const mongoose = require('mongoose')
 const multer = require('multer')
